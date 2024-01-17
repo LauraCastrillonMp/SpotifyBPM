0f67901c6bdef2fb08ddfe18b3f90fc3dda9a8fc
LauraCastrillonMp <lauracastrillon.mp@gmail.com>
2024-01-16 21:27:19 -0500

    root file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Screen1 from './routes/Screen1'
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Screen1 />
   </React.StrictMode>
 )
