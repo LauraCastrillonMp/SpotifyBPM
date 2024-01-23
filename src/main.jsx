88f89a6ec6596502f2a962a656ee7bdad3ed69b9
LauraCastrillonMp <lauracastrillon.mp@gmail.com>
2024-01-23 17:53:26 -0500

    A bit of react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Screen1 from './routes/Screen1'
-import './styles/index.css'
+import Screen1 from 'routes/Screen1'
+import ErrorPageLanding from "containers/error-pages/error-page-landing";
+import Screen2 from 'routes/Screen2';
+import Screen3 from 'routes/Screen3';
+import Screen4 from 'routes/Screen4';
+import 'styles/index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Screen1 />,
+    errorElement: <ErrorPageLanding />,
+  },
+  {
+    path: "screen2",
+    element: <Screen2 />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Screen1 />
+     <RouterProvider router={router} />
   </React.StrictMode>
 )
