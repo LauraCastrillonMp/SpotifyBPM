7f1b75f241a0e97b388aa5fdcb9db0ae3930236f
LauraCastrillonMp <lauracastrillon.mp@gmail.com>
2024-01-27 23:31:05 -0500

    Creation of project on Nextjs and Shadcn

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import "@/styles/globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Inter as FontSans } from "next/font/google";
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+import { cn } from "@/lib/utils";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
