461abd50e938d85ee5e8bf255bbac7bc9f12f3de
LauraCastrillonMp <lauracastrillon.mp@gmail.com>
2024-01-29 17:12:47 -0500

    Site Config

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "spotify-playlist-to",
+  url: "https://ui.shadcn.com",
+  ogImage: "https://ui.shadcn.com/og.jpg",
+  description:
+    "The Best Transfer Spotify Playlist To Any Music Streaming Service. Your favorite playlists in one place.",
+  links: {
+    twitter: "https://twitter.com/LauraMaraC40352",
+    github: "https://github.com/LauraCastrillonMp/spotify-playlist-to",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
