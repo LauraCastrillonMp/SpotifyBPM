92d6390a4c04da01fb69a99d2a01ec6f8293e63a
LauraCastrillonMp <lauracastrillon.mp@gmail.com>
2024-01-29 21:36:47 -0500

    Some text for footer

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -2,10 +2,10 @@ import { siteConfig } from "@/config/site"
 
 export function SiteFooter() {
   return (
-    <footer className="py-6 md:px-8 md:py-0">
-      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+    <footer className="py-4 md:px-8 md:py-0">
+      <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
         <p className="text-balance text-center text-sm leading-loose text-gray-500 md:text-left">
-          Built by{" "}
+          Built with ❤ by{" "}
           <a
             href={siteConfig.links.twitter}
             target="_blank"
