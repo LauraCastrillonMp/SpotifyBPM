463b36f772ec9f02dd92454952fd20482e1658be
LauraCastrillonMp <lauracastrillon.mp@gmail.com>
2024-03-11 21:17:54 -0500

    Some functions for Auth

--- app/transfer/page.tsx
+++ app/transfer/page.tsx
@@ -2,8 +2,17 @@ import {
   SpotifyIconButton,
   YouTubeMusicIconButton,
 } from "@/components/ui/icon-buttons";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/utils/auth";
 
-export default function Transfer() {
+export default async function Transfer() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    return redirect("/transfer");
+  }
+
   return (
     <section className="w-full flex flex-col items-center justify-center px-2 gap-4 md:px-8 py-44">
       <p className="text-xl font-semibold">Choose the source platform</p>
