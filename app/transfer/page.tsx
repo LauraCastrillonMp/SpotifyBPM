7fa5a2c7f7ab7b3fa933a4179880e250bea15cdf
LauraCastrillonMp <lauracastrillon.mp@gmail.com>
2024-03-11 21:17:06 -0500

    Page for available services

---
+++ app/transfer/page.tsx
@@ -0,0 +1,16 @@
+import {
+  SpotifyIconButton,
+  YouTubeMusicIconButton,
+} from "@/components/ui/icon-buttons";
+
+export default function Transfer() {
+  return (
+    <section className="w-full flex flex-col items-center justify-center px-2 gap-4 md:px-8 py-44">
+      <p className="text-xl font-semibold">Choose the source platform</p>
+      <div className="grid grid-cols-2 gap-4">
+        <SpotifyIconButton />
+        <YouTubeMusicIconButton />
+      </div>
+    </section>
+  );
+}
