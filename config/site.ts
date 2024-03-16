17ad8c480556a773785302e8d5f984566f952f24
LauraCastrillonMp <lauracastrillon.mp@gmail.com>
2024-03-15 22:52:58 -0500

    some more links

--- config/site.ts
+++ config/site.ts
@@ -1,3 +1,5 @@
+import { signIn } from "next-auth/react";
+
 export const siteConfig = {
   name: "spotify-playlist-to",
   url: "https://ui.shadcn.com",
@@ -5,6 +7,8 @@ export const siteConfig = {
   description:
     "The Best Transfer Spotify Playlist To Any Music Streaming Service. Your favorite playlists in one place.",
   links: {
+    home: "/",
+    signIn: "/api/auth/signin",
     twitter: "https://twitter.com/LauraMaraC40352",
     github: "https://github.com/LauraCastrillonMp/spotify-playlist-to",
   },
